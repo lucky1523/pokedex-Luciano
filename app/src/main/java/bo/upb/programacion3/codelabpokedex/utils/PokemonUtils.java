981982ab99fc90adbe1f941eeddfbaa2878a3c63
luciano <erickmallea@hotmail.com>
2020-06-24 16:33:01 -0500

    pokedex-luciano

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -26,7 +26,16 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
         pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
-        pokemons.add(new Pokemon(13, "nepe", R.drawable.zubat, "Poison"));
+        pokemons.add(new Pokemon(13, "Dialga", R.drawable.dialga, "legendary"));
+        pokemons.add(new Pokemon(13, "zekrom", R.drawable.zekrom, "legendary"));
+        pokemons.add(new Pokemon(14, "aerodactyl", R.drawable.aerodactyl, "dragon"));
+        pokemons.add(new Pokemon(15, "latias", R.drawable.latias, "dragon"));
+        pokemons.add(new Pokemon(16, "latios", R.drawable.latios, "dragon"));
+        pokemons.add(new Pokemon(17, "mewtwo", R.drawable.mewtwo, "legendary"));
+        pokemons.add(new Pokemon(18, "skarmory", R.drawable.skarmory, "steel"));
+        pokemons.add(new Pokemon(19, "metagross", R.drawable.metagross1, "steel"));
+        pokemons.add(new Pokemon(20, "reshiram", R.drawable.reshiram, "legendary"));
+        pokemons.add(new Pokemon(21, "giratina", R.drawable.giratina1, "legendary"));
         return pokemons;
     }
 }
